8f4df57065d87d7f0778b80c44de72cdfb3c2417
Crowdbotics <team@crowdbotics.com>
2023-08-25 00:19:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-shadow-43061",
+  url: "https://steep-shadow-43061.botics.co",
+  api: "https://steep-shadow-43061.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
